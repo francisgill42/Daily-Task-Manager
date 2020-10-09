9059adfd0c89b5324f9d2d529168a67e75778274
francisgill42 <francisgill42@gmail.com>
2020-10-10 02:33:58 +0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app dark>
+    <v-app-bar app>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+   
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      fixed: false,
+      title: 'Daily Task Manager'
+    }
+  }
+}
+</script>
